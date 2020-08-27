0de20572ee41da84d9ed1334ea31db8045bb1fde
Chris Ramer <chrisramer.97@gmail.com>
2020-08-27 15:45:29 -0700

    Add first set of checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-	
-	
-
+	$("form#transportation_survey").submit(function (event) {
+		event.preventDefault();
+		$("#output").show();
+		$("input:checkbox[name=work-transportation]:checked").each(function () {
+			const workTransportationMode = $(this).val();
+			$("#output").append(workTransportationMode + "<br>");
+		});
+		$('#transportation_survey').hide();
+	});
 });
